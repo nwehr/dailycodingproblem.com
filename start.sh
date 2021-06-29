17d7eb2233898b13c263a907f084df66d7641cd7
Nathan Wehr <nathan@wehrholdings.com>
2021-06-29 11:33:36 -0400

    completed 23, 24, 25 and 26

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker run -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/scipy-notebook
+
+# docker run -e JUPYTER_PATH=/home/jovyan/work -p 8888:8888 -v $(pwd):/home/jovyan/work gopherdata/gophernotes:latest-ds
